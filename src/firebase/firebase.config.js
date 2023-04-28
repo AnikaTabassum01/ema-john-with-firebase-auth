9730b8648a54cff62606e011db83041a5a9b9082
Anika <anika.bd1994@gmail.com>
2023-04-28 22:56:47 +0600

    login and signup form added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnQKciHiyj8RmlZDi-qu7Tfw7lxjtqzm4",
+  authDomain: "ema-john-with-firebase-a-2ba88.firebaseapp.com",
+  projectId: "ema-john-with-firebase-a-2ba88",
+  storageBucket: "ema-john-with-firebase-a-2ba88.appspot.com",
+  messagingSenderId: "834575613777",
+  appId: "1:834575613777:web:689df9f03c6640684add70"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
